192fa1d2488def2f60c3fd8d9adfc7b96edde73c
glebite <glebite@gmail.com>
2023-04-23 15:54:21 -0400

    Adding the base of stuff...

---
+++ atash/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template, request
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', pageTitle='آتش')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file-input']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
