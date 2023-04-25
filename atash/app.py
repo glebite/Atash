60a5143fb02dc285e1efa1b74c424a80abfc58b6
glebite <glebite@gmail.com>
2023-04-24 21:37:04 -0400

    Trying some things out.

--- atash/app.py
+++ atash/app.py
@@ -1,18 +1,25 @@
+"""app.py
+"""
 from flask import Flask
 from flask import render_template, request
 from werkzeug.utils import secure_filename
 
 
+# instantiate the application
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """index
+    """
     return render_template('index.html', pageTitle='آتش')
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
+    """upload_file
+    """
     if request.method == 'POST':
         f = request.files['file-input']
         f.save(secure_filename(f.filename))
