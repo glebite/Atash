759ad350c2e0e5103889745e69929d7bc32978ed
glebite <glebite@gmail.com>
2023-04-30 22:01:40 -0400

    Better stats attempt.

--- atash/atash.py
+++ atash/atash.py
@@ -4,6 +4,17 @@ Subclassing Flask to Atash will allow for the retention
 of state, etc...
 """
 import flask
+from functools import wraps
+from collections import defaultdict
+
+
+def stats_wrapper(method):
+    @wraps(method)
+    def _impl(self, *method_args, **method_kwargs):
+        method_output = method(self, *method_args, **method_kwargs)
+        self.requests_counter[method.__name__] += 1
+        return method_output
+    return _impl
 
 
 class Atash(flask.Flask):
@@ -20,17 +31,25 @@ class Atash(flask.Flask):
         """
         """
         self.route('/stats')(self.stats)
+        self.route('/caller')(self.caller)
 
+    @stats_wrapper
     def stats(self):
         """
         """
-        self.requests_counter['stats'] += 1
         return "stats"
+
+    @stats_wrapper
+    def caller(self):
+        """
+        """
+        return str(self.requests_counter)
 
     def build_stats(self):
         """
         """
-        self.requests_counter = dict()
+        self.requests_counter = defaultdict(lambda: 0)
+        return
 
 
 if __name__ == "__main__":
